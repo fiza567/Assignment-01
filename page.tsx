0335e441d827a2495fb03e913a49b47cdae78ce7
fiza567 <159537826+fiza567@users.noreply.github.com>
2024-11-01 22:38:25 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,18 +1,12 @@
-import React from 'react'
-import Link from 'next/link'
-import styles from "../service/service.module.css"
-const ServicePage = () => {
-  return (
-    <div className={styles.container}>
-        <h1 className={styles.header}>This is service page </h1>
-        <ul>
-<li><Link href="/service/graphic-design">This is graphic designing page</Link></li>
-<li>< Link href="/service/video-editing">This is video editing page</Link ></li>
+import Image from "next/image";
+import Link from "next/link";
 
+export default function Home() {
+return(
+  <div>
+    <img src="https://blog.logrocket.com/wp-content/uploads/2023/01/import-svgs-next-js-app.png " alt="sorry" />
+  </div>
+  
+)         
 
-        </ul>
-    </div>
-  )
-}
-
-export default ServicePage
+} 
